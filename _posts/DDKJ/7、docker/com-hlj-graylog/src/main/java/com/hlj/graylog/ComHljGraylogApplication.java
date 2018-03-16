3b3207b2b7d91140a1475b6b5e6f8ddd2d2e44d2
zhangyj123 <yujin.zhang@duodian.com>
2018-03-16 15:52:24 +0800

    graylgo

---
+++ _posts/DDKJ/7、docker/com-hlj-graylog/src/main/java/com/hlj/graylog/ComHljGraylogApplication.java
@@ -0,0 +1,12 @@
+package com.hlj.graylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComHljGraylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComHljGraylogApplication.class, args);
+	}
+}
