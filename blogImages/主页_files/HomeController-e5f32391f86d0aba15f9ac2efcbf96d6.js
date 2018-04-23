6a6438a054da47b6105f56874da36d244e50f920
HealerJean <mxzdhealer@gmail.com>
2018-04-23 23:47:45 +0800

    sentinel

--- blogImages/主页_files/HomeController-e5f32391f86d0aba15f9ac2efcbf96d6.js
+++
@@ -1,3 +0,0 @@
-var HomeController = app.controller('HomeController', function($rootScope,$scope,$http,$routeParams,$location,ngDialog) {
-    // adUrlFind('/business#/home',window.location.origin);
-});
