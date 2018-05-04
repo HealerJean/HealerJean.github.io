aa3127829ebb71ba3ad01d453cfd0cfca0b0cc40
zhangyj123 <yujin.zhang@duodian.com>
2018-05-04 18:34:26 +0800

    docker ui

---
+++ _posts/DDKJ/4、mybatis和hibernate/2018_04_26_7_springBoot继承mybatis实现类似JPA方法/com-hlj-mybatis-Automatic-method/src/main/java/com/hlj/mybatisxml/entity/baseset/MyBatisCodeHelperBean.java
@@ -0,0 +1,28 @@
+package com.hlj.mybatisxml.entity.baseset;
+
+/**
+ * @Description
+ * @Author HealerJean
+ * @Date 2018/5/4  下午12:21.
+ */
+public class MyBatisCodeHelperBean {
+
+    int aget ;
+    String name ;
+
+    public int getAget() {
+        return aget;
+    }
+
+    public void setAget(int aget) {
+        this.aget = aget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
