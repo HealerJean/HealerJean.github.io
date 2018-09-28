541934fe73cf1b7ea78c5e25bfe849fa4c0c0942
zhangyj123 <yujin.zhang@duodian.com>
2018-09-28 11:08:41 +0800

    markdown

--- _posts/DDKJ/6、工具类/28、插件代码/1_markdown_springboot/2018_09_28_1_SpringBoot编辑Markdown/editor-markdown/src/main/java/cn/merryyou/editor/editor/repository/EditorRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.merryyou.editor.editor.repository;
-
-import cn.merryyou.editor.editor.domain.Editor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EditorRepository extends JpaRepository<Editor, Integer> {
-}
