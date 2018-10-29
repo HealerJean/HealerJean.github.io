fba59135fe2d656155675b767ac8f888324a3884
HealerJean123 <mxzdhealer@gmail.com>
2018-10-29 10:11:52 +0800

    css

--- _posts/DDKJ/20、linux/16、shell启动停止重启SpingBoot_Jar并生成日志文件/start.sh
+++ _posts/DDKJ/20、linux/16、shell启动停止重启SpingBoot_Jar并生成日志文件/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
    echo starting
-   java -jar admin-1.0-SNAPSHOT.jar --spring.profiles.active=test  > log.file 2>log.error &
+   java -jar admin-1.0-SNAPSHOT.jar --spring.profiles.active=ihaidou  > log.file 2>log.error &
