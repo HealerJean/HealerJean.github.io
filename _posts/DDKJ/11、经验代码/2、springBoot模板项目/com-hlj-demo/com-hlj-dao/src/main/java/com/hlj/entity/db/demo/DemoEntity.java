f101ed0e9dc55e6bfcb1b7f4d8bcd90944779c66
HealerJean123 <mxzdhealer@gmail.com>
2018-11-14 15:33:39 +0800

    经验代码

--- _posts/DDKJ/11、经验代码/2、springBoot模板项目/com-hlj-demo/com-hlj-dao/src/main/java/com/hlj/entity/db/demo/DemoEntity.java
+++ _posts/DDKJ/11、经验代码/2、springBoot模板项目/com-hlj-demo/com-hlj-dao/src/main/java/com/hlj/entity/db/demo/DemoEntity.java
@@ -4,9 +4,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 测试实体类;
@@ -29,14 +31,27 @@ public class DemoEntity{
 	@ApiModelProperty(value = "年龄")
 	private Long age ;
 
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP",insertable = true,updatable = false)
+	private Date cdate;
+
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date udate;
+
 }
 
 /**
+
+ drop table demo_entity;
 
  CREATE TABLE `demo_entity` (
  `id` bigint(20) NOT NULL AUTO_INCREMENT,
  `name` varchar(255)   DEFAULT NULL,
  `age` bigint(20) DEFAULT '0',
+ `cdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `udate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  PRIMARY KEY (`id`)
  );
  */
