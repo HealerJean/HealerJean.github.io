e625c312fda986f595be3b2d8730d8a4a29adce5
HealerJean <healerjean@gmail.com>
2018-12-04 14:32:44 +0800

    lombok

---
+++ _posts/DDKJ/11、经验代码/2、springBoot模板项目/com-hlj-nosql-demo/com-hlj-admin/src/main/java/com/hlj/moudle/demo/service/impl/DemoEntityServiceImpl.java
@@ -0,0 +1,18 @@
+package com.hlj.moudle.demo.service.impl;
+
+import com.hlj.moudle.demo.service.DemoEntityService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @Desc:
+ * @Author HealerJean
+ * @Date 2018/9/17  下午2:39.
+ */
+@Service
+@Slf4j
+public class DemoEntityServiceImpl implements DemoEntityService {
+
+
+}
---
+++ _posts/DDKJ/11、经验代码/2、springBoot模板项目/com-hlj-nosql-demo/readme.md
@@ -0,0 +1,14 @@
+
+
+
+
+#### 添加实体
+
+[http://localhost:8080/demo/addDemoEntity?name=HealerJean](http://localhost:8080/demo/addDemoEntity?name=HealerJean)
+
+#### 根据id查找实体
+
+[http://localhost:8080/demo/findById?id=1](http://localhost:8080/demo/findById?id=1)
+
+#### 查看所有的实体类
+[http://localhost:8080/demo/findAll](http://localhost:8080/demo/findAll)
