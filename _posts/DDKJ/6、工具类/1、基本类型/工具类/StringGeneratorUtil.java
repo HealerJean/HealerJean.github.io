a6b1d2aedd8809351e2509f1952f23a1174865dc
HealerJean <healerjean@gmail.com>
2018-12-27 19:00:23 +0800

    string根据字数分隔，java根据像素分组

--- _posts/DDKJ/6、工具类/1、基本类型/工具类/StringGeneratorUtil.java
+++ _posts/DDKJ/6、工具类/1、基本类型/工具类/StringGeneratorUtil.java
@@ -2,9 +2,7 @@ package com.duodian.youhui.admin.utils;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.util.Date;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -277,6 +275,61 @@ public class StringGeneratorUtil {
             }
         }
         return false;
+    }
+
+
+
+
+
+
+
+    /**
+     * 把原始字符串分割成指定长度的字符串列表
+     * @param inputString  原始字符串
+     * @param length   指定长度
+     * @return
+     */
+    public static List<String> getStrList(String inputString, int length) {
+        int size = inputString.length() / length;
+        if (inputString.length() % length != 0) {
+            size += 1;
+        }
+        return getStrList(inputString, length, size);
+    }
+
+
+    /**
+     * 把原始字符串分割成指定长度的字符串列表
+     * @param inputString 原始字符串
+     * @param length 指定长度
+     * @param size  指定列表大小
+     * @return
+     */
+    public static List<String> getStrList(String inputString, int length,
+                                          int size) {
+        List<String> list = new ArrayList<String>();
+        for (int index = 0; index < size; index++) {
+            String childStr = substring(inputString, index * length,
+                    (index + 1) * length);
+            list.add(childStr);
+        }
+        return list;
+    }
+    /**
+     * 分割字符串，如果开始位置大于字符串长度，返回空
+     * @param str  原始字符串
+     * @param f 开始位置
+     * @param t  结束位置
+     * @return
+     */
+    public static String substring(String str, int f, int t) {
+        if (f > str.length())
+            return null;
+        if (t > str.length()) {
+            return str.substring(f, str.length());
+        } else {
+            return str.substring(f, t);
+        }
     }
 
 
