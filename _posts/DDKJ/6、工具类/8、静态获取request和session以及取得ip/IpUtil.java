a951a18a656a5c54828f004a1f7badb16790e116
HealerJean <mxzdhealer@gmail.com>
2019-06-16 23:59:57 +0800

    json

--- _posts/DDKJ/6、工具类/8、静态获取request和session以及取得ip/IpUtil.java
+++ _posts/DDKJ/6、工具类/8、静态获取request和session以及取得ip/IpUtil.java
@@ -76,4 +76,53 @@ public class IpUtil {
             return  null;
         }
     }
+
+
+
+     /**
+     * 获取调用的域名
+     * @param urlTarget
+     * @return
+     */
+    public static String getDomainAndPort(String urlTarget)
+    {
+        //跳转到对应的回调地址
+        String domain = "";
+        try {
+            URL url = new URL(urlTarget);
+            String host = url.getHost();
+            int port = url.getPort();
+            String s = url.toString();
+            domain = s.substring(0,s.indexOf(host)+host.length());
+            if(port != -1) {
+                domain = domain + ":" + port;
+            }
+        } catch (MalformedURLException e) {
+            log.info("获取域名失败");
+        }
+        return domain ;
+    }
+
+    public static String genUrl(String hostPort, List<String> uriSegment , Map<String,String> params) {
+        HttpUrl httpUrl = HttpUrl.parse(hostPort);
+        HttpUrl.Builder builder = httpUrl.newBuilder();
+        if(uriSegment != null && !uriSegment.isEmpty()){
+            for (String segment: uriSegment) {
+                builder.addPathSegment(segment);
+            }
+        }
+        if(params != null && !params.isEmpty()) {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                builder.addEncodedQueryParameter(entry.getKey(), entry.getValue());
+            }
+        }
+        httpUrl = builder.build();
+        return httpUrl.toString();
+    }
+
+    public static StringBuffer getRealRequestURL(HttpServletRequest request){
+
+        StringBuffer requestURL = request.getRequestURL();
+        return requestURL;
+    }
 }
