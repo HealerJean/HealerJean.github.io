7c086eb835f6ac59f29821ad29dc58c48cfdf268
HealerJean <mxzdhealer@gmail.com>
2019-10-27 17:15:49 +0800

    nginx

---
+++ _posts/DDKJ/22、nginx/hlj-nginx/hlj-parent/hlj-nginx/src/main/java/com/healerjean/proj/NginxApplication.java
@@ -0,0 +1,15 @@
+package com.healerjean.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NginxApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginxApplication.class, args);
+    }
+
+}
