fcd89b928d8a4ead046f7f245701d4c017b89833
HealerJean <mxzdhealer@gmail.com>
2019-10-28 23:10:10 +0800

    demo

---
+++ _posts/4_Demo代码/hlj-rsa/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/a_test/dto/MessageInfo.java
@@ -0,0 +1,16 @@
+package com.healerjean.proj.a_test.dto;
+
+import lombok.Data;
+
+/**
+ * @author HealerJean
+ * @ClassName MessageInfo
+ * @date 2019-10-28  23:07.
+ * @Description
+ */
+@Data
+public class MessageInfo {
+
+
+
+}
