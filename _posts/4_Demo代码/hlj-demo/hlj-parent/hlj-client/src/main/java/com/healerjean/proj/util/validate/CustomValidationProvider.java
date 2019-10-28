2cd47e49845393948966ab90312eab872ab2fd22
HealerJean <healerjean@gmail.com>
2019-10-28 22:36:04 +0800

    hlj-demo

---
+++ _posts/4_Demo代码/hlj-demo/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/util/validate/CustomValidationProvider.java
@@ -0,0 +1,34 @@
+package com.healerjean.proj.util.validate;
+
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.internal.engine.ConfigurationImpl;
+
+import javax.validation.Configuration;
+import javax.validation.ValidatorFactory;
+import javax.validation.spi.BootstrapState;
+import javax.validation.spi.ConfigurationState;
+import javax.validation.spi.ValidationProvider;
+
+/**
+ * @author HealerJean
+ * @ClassName CustomValidationProvider
+ * @date 2019/8/9  14:54.
+ * @Description
+ */
+public class CustomValidationProvider implements ValidationProvider<HibernateValidatorConfiguration> {
+
+    @Override
+    public HibernateValidatorConfiguration createSpecializedConfiguration(BootstrapState state) {
+        return HibernateValidatorConfiguration.class.cast(new ConfigurationImpl(this));
+    }
+
+    @Override
+    public Configuration<?> createGenericConfiguration(BootstrapState state) {
+        return new ConfigurationImpl(state);
+    }
+
+    @Override
+    public ValidatorFactory buildValidatorFactory(ConfigurationState configurationState) {
+        return new CustomValidatorFactoryImpl(configurationState);
+    }
+}
