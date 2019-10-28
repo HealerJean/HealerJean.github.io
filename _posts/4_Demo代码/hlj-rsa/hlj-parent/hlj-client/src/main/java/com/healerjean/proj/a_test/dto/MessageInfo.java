1374fc70c774a4bd40ee5eddea677e376a848fd3
HealerJean <mxzdhealer@gmail.com>
2019-10-28 23:39:36 +0800

    json

--- _posts/4_Demo代码/hlj-rsa/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/a_test/dto/MessageInfo.java
+++ _posts/4_Demo代码/hlj-rsa/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/a_test/dto/MessageInfo.java
@@ -1,5 +1,6 @@
 package com.healerjean.proj.a_test.dto;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 
 /**
@@ -8,9 +9,15 @@ import lombok.Data;
  * @date 2019-10-28  23:07.
  * @Description
  */
+@JacksonXmlRootElement(localName = "MESSAGE_INFO")
 @Data
 public class MessageInfo {
 
 
+    private int retCode ;
+
+    private String retMsg;
+
+    private String signedMsg ;
 
 }
