a05a3ad856e681b929f4c97d9aaec367b0864cbb
HealerJean <healerjean@gmail.com>
2019-12-04 14:55:14 +0800

    更新异常信息

---
+++ _posts/3_工具类/5_项目规范通用解决方式/exception/ExistException.java
@@ -0,0 +1,13 @@
+package com.healerjean.proj.common.exception;
+
+
+import com.healerjean.proj.common.enums.ResponseEnum;
+
+
+public class ExistException extends BusinessException {
+
+    public ExistException(ResponseEnum  responseEnum) {
+        super(responseEnum);
+    }
+
+}
---
+++ _posts/4_Demo代码/4_one_nosql_demo/hlj-demo/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/common/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.healerjean.proj.common.exception;
+
+
+import com.healerjean.proj.common.enums.ResponseEnum;
+
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(ResponseEnum  responseEnum) {
+        super(responseEnum);
+    }
+
+}
