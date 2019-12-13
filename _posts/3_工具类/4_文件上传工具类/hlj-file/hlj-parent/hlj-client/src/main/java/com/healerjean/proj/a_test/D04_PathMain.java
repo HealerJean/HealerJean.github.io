6614953e3980fa8c16253763ff5ebe5402a75b68
HealerJean <healerjean@gmail.com>
2019-12-13 11:08:18 +0800

    resource

---
+++ _posts/3_工具类/4_文件上传工具类/hlj-file/hlj-parent/hlj-client/src/main/java/com/healerjean/proj/a_test/D04_PathMain.java
@@ -0,0 +1,19 @@
+package com.healerjean.proj.a_test;
+
+import org.junit.Test;
+
+/**
+ * @author HealerJean
+ * @ClassName D04_PathMain
+ * @date 2019/12/13  10:50.
+ * @Description
+ */
+public class D04_PathMain {
+
+    @Test
+    public void testClassPath() {
+        System.out.println(this.getClass().getResource(""));
+        System.out.println(this.getClass().getResource("/"));
+    }
+
+}
