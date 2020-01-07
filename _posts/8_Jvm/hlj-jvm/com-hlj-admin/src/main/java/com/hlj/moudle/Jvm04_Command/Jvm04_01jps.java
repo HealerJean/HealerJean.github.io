8a388004450073367f55522df7cb815b154dd9b6
HealerJean <healerjean@gmail.com>
2020-01-07 20:27:35 +0800

    Jvm马上完成了

---
+++ _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm04_Command/Jvm04_01jps.java
@@ -0,0 +1,15 @@
+package com.hlj.moudle.Jvm04_Command;
+import java.util.Scanner;
+
+public class Jvm04_01jps {
+
+    public static void main(String[] args) throws InterruptedException {
+       method();
+    }
+
+    public static void method(){
+        while (true) {
+
+        }
+    }
+}
