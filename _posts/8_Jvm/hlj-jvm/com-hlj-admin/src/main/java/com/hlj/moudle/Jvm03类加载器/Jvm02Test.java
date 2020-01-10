86841a3382ab4c6d42b069c7ed30d60c174e1faa
HealerJean <healerjean@gmail.com>
2020-01-10 16:45:18 +0800

    文件

--- _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm03类加载器/Jvm02Test.java
+++ _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm03类加载器/Jvm02Test.java
@@ -13,4 +13,5 @@ public class Jvm02Test {
     }
 
 
+
 }
--- _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm03类加载器/Jvm03ClassLoaderTest.java
+++ _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm03类加载器/Jvm03ClassLoaderTest.java
@@ -10,35 +10,27 @@ import java.lang.reflect.Method;
  */
 public class Jvm03ClassLoaderTest {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception {
 
         //创建自定义classloader对象。
-        Jvm03DiskClassLoader diskLoader = new Jvm03DiskClassLoader("D:\\study\\HealerJean.github.io\\_posts\\8_Jvm\\hlj-jvm\\com-hlj-admin\\src\\main\\java");
-        try {
-            //加载class文件
-            Class c = diskLoader.loadClass("com.hlj.moudle.Jvm03类加载器.Jvm02Test");
-            System.out.println(diskLoader.getParent());
-            if (c != null) {
-                try {
-                    Object obj = c.newInstance();
-                    Method method = c.getDeclaredMethod("say", null);
-                    //通过反射调用Test类的say方法
-                    method.invoke(obj, null);
-                } catch (InstantiationException | IllegalAccessException
-                        | NoSuchMethodException
-                        | SecurityException |
-                        IllegalArgumentException |
-                        InvocationTargetException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        Jvm03DiskClassLoader loader = new Jvm03DiskClassLoader("D:\\study\\HealerJean.github.io\\_posts\\8_Jvm\\hlj-jvm\\com-hlj-admin\\src\\main\\java");
+        System.out.println(loader);
+        //加载class文件
+        Class c = loader.loadClass("com.hlj.moudle.Jvm03类加载器.Jvm02Test");
+        System.out.println(loader.getParent());
+        Object obj = c.newInstance();
+        Method method = c.getDeclaredMethod("say", null);
+        //通过反射调用Test类的say方法
+        method.invoke(obj, null);
 
+        //清除该类的实例
+        obj = null;
+        //清除该类的ClassLoader引用
+        loader = null;
+        //清除该class对象的引用
+        c = null;
+        // 执行一次gc垃圾回收
+        System.gc();
     }
 
 }
