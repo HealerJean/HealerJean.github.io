d07e8b3f8f36d1f255426e40876a1fb3d393ee60
HealerJean <healerjean@gmail.com>
2020-02-25 20:13:15 +0800

    jdbc 工具类

--- _posts/3_工具类/6_Excel导出_导入/导入/POIExcelHelper.java
+++ _posts/3_工具类/6_Excel导出_导入/导入/POIExcelHelper.java
@@ -1,4 +1,4 @@
-package com.duodian.admore.admin.utils;
+package com.healerjean.proj.util.excel;
 
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -8,63 +8,37 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 类名称：POIExcelUtil
- * 类描述：
- * 创建人：shiwei.li
- * 修改人：
- * 修改时间：2015/7/21 16:51
- * 修改备注：
- *
- * @version 1.0.0
+ * @author HealerJean
+ * @ClassName POIExcelHelper
+ * @Date 2020/2/25  19:37.
+ * @Description
  */
 public class POIExcelHelper {
 
-    private int totalRows = 0;// 总行数
-    private int totalCells = 0;// 总列数
+    // 总行数
+    private int totalRows = 0;
+    // 总列数
+    private int totalCells = 0;
 
     public POIExcelHelper() {
     }
 
-    // 根据文件名读取excel文件
-    public List<ArrayList<String>> read(String fileName) {
-        List<ArrayList<String>> dataLst = new ArrayList<ArrayList<String>>();
-
-        // 检查文件名是否为空或者是否是Excel格式的文件
-        if (fileName == null || !fileName.matches("^.+\\.(?i)((xls)|(xlsx))$")) {
-            return dataLst;
-        }
-
-        boolean isExcel2003 = true;
-        // 对文件的合法性进行验
-        if (fileName.matches("^.+\\.(?i)(xlsx)$")) {
-            isExcel2003 = false;
-        }
-
-        // 检查文件是否存在
-        File file = new File(fileName);
-        if (file == null || !file.exists()) {
-            return dataLst;
-        }
-
-        try {
-            // 读取excel
-            dataLst = read(new FileInputStream(file), isExcel2003);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return dataLst;
-    }
-
-    // 根据流读取Excel文件
+    /**
+     * 根据流读取Excel文件
+     *
+     * @param inputStream 流
+     * @param isExcel2003 是否2003版本
+     * @return
+     */
     public List<ArrayList<String>> read(InputStream inputStream, boolean isExcel2003) {
         List<ArrayList<String>> dataLst = null;
         try {
@@ -77,13 +51,13 @@ public class POIExcelHelper {
         return dataLst;
     }
 
-    // 得到总行数
-    public int getTotalRows() {
+    //得到总行数
+    private int getTotalRows() {
         return totalRows;
     }
 
-    // 得到总列数
-    public int getTotalCells() {
+    //得到总列数
+    private int getTotalCells() {
         return totalCells;
     }
 
@@ -91,7 +65,8 @@ public class POIExcelHelper {
     private List<ArrayList<String>> read(Workbook wb) {
         List<ArrayList<String>> dataLst = new ArrayList<ArrayList<String>>();
 
-        Sheet sheet = wb.getSheetAt(0);// 得到第一个shell
+        // 得到第一个shell
+        Sheet sheet = wb.getSheetAt(0);
         this.totalRows = sheet.getPhysicalNumberOfRows();
         if (this.totalRows >= 1 && sheet.getRow(0) != null) {
             this.totalCells = sheet.getRow(0).getPhysicalNumberOfCells();
@@ -105,7 +80,6 @@ public class POIExcelHelper {
             }
 
             ArrayList<String> rowLst = new ArrayList<String>();
-
             // 循环Excel的列
             for (short c = 0; c < this.getTotalCells(); c++) {
                 Cell cell = row.getCell(c);
@@ -115,20 +89,24 @@ public class POIExcelHelper {
                     continue;
                 }
 
-                // 处理数字型的,自动去零
-                if (Cell.CELL_TYPE_NUMERIC == cell.getCellType()) {
 
+                if (Cell.CELL_TYPE_NUMERIC == cell.getCellType()) {
                     // 在excel里,日期也是数字,在此要进行判断
                     if (HSSFDateUtil.isCellDateFormatted(cell)) {
-                        cellValue = DateHelper.convertDate2String(cell.getDateCellValue());
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        cellValue = dateFormat.format(cell.getDateCellValue());
                     } else {
                         cellValue = getRightStr(cell.getNumericCellValue() + "");
                     }
-                } else if (Cell.CELL_TYPE_STRING == cell.getCellType()) {// 处理字符串型
+                }
+                // 处理字符串型
+                else if (Cell.CELL_TYPE_STRING == cell.getCellType()) {
                     cellValue = cell.getStringCellValue();
-                } else if (Cell.CELL_TYPE_BOOLEAN == cell.getCellType()) {// 处理布尔型
+                    // 处理布尔型
+                } else if (Cell.CELL_TYPE_BOOLEAN == cell.getCellType()) {
                     cellValue = cell.getBooleanCellValue() + "";
-                } else {// 其它数据类型
+                    // 其它数据类型
+                } else {
                     cellValue = cell.toString() + "";
                 }
 
@@ -142,32 +120,14 @@ public class POIExcelHelper {
     // 正确地处理整数后自动加零的情况
     private String getRightStr(String sNum) {
         DecimalFormat decimalFormat = new DecimalFormat("#.000000");
-        String resultStr = decimalFormat.format(new Double(sNum));
+        String resultStr = decimalFormat.format(Double.valueOf(sNum));
         if (resultStr.matches("^[-+]?\\d+\\.[0]+$")) {
             resultStr = resultStr.substring(0, resultStr.indexOf("."));
+        } else if (new BigDecimal(sNum).compareTo(BigDecimal.ZERO) == 0) {
+            return "0";
         }
         return resultStr;
     }
 
-//    public static void main(String[] args) throws Exception {
-//
-//        String fileName = "E:\\ExcelDemo\\电子资产使用情况.xls";
-//        POIExcelHelper excelUtil = new POIExcelHelper();
-//        List<ArrayList<String>> dataLst = excelUtil.read(fileName);// 读取excle，并把数据打包成list
-//
-//        System.out.println("	--------------------------excel内容如下--------------------------------");
-//
-//        for (ArrayList<String> innerLst : dataLst) {
-//            StringBuffer rowData = new StringBuffer();
-//            for (String dataStr : innerLst) {
-//                rowData.append(",").append(dataStr);
-//            }
-//            if (rowData.length() > 0) {
-//                System.out.println("	" + rowData.deleteCharAt(0).toString());
-//            }
-//        }
-//
-//        System.out.println("	总行数：" + excelUtil.getTotalRows() + " , 总列数：" + excelUtil.getTotalCells());
-//
-//    }
+
 }
