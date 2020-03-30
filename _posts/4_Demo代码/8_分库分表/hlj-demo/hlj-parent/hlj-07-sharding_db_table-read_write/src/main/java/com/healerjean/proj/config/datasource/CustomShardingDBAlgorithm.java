23fabd8fc6ff50e6a1528e0b5d03482fcb53c4a9
HealerJean <healerjean@gmail.com>
2020-03-30 17:56:45 +0800

    开始重点测试

---
+++ _posts/4_Demo代码/8_分库分表/hlj-demo/hlj-parent/hlj-07-sharding_db_table-read_write/src/main/java/com/healerjean/proj/config/datasource/CustomShardingDBAlgorithm.java
@@ -0,0 +1,29 @@
+package com.healerjean.proj.config.datasource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
+import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
+
+import java.util.Collection;
+
+/**
+ * @author HealerJean
+ * @ClassName CustomShardingDBAlgorithm
+ * @date 2020-03-29  20:48.
+ * @Description
+ */
+@Slf4j
+public class CustomShardingDBAlgorithm implements PreciseShardingAlgorithm<Long> {
+
+
+    @Override
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Long> shardingValue) {
+        for (String dbName : availableTargetNames) {
+            if (dbName.endsWith(shardingValue.getValue() % 2 + "")) {
+                log.info("库为：{}, 主键为：{}, 最终被分到的库为：【{}】", availableTargetNames, shardingValue, dbName);
+                return dbName;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
