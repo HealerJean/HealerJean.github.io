53135f788f7f0d30161fe6fb7b32b64f42b0cb29
HealerJean <healerjean@gmail.com>
2020-06-02 15:13:58 +0800

    和位k的连续子数组

--- _posts/7_SpringBoot/spirng-aop/src/main/java/com/healerjean/proj/SpirngAopApplication.java
+++ _posts/7_SpringBoot/spirng-aop/src/main/java/com/healerjean/proj/SpirngAopApplication.java
@@ -4,8 +4,12 @@ import com.healerjean.proj.service.UserService;
 import com.healerjean.proj.service.impl.UserServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * 想要分析SpringBoot的启动过程，我们需要熟悉SpringApplication的构造过程以及SpringApplication的run方法执行过程即可。
+ */
 @SpringBootApplication
 public class SpirngAopApplication {
 
@@ -15,7 +19,7 @@ public class SpirngAopApplication {
         //AnnotationConfigApplicationContext 是基于注解来使用的，它不需要配置文件，
         //采用 java 配置类和各种注解来配置，是比较简单的方式，也是大势所趋吧。
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(SpirngAopApplication.class);
-
+        ApplicationContextInitializer
         // 从 context 中取出我们的 Bean，而不是用 new UserService() 这种方式
         UserService userService = annotationConfigApplicationContext.getBean(UserService.class);
 
