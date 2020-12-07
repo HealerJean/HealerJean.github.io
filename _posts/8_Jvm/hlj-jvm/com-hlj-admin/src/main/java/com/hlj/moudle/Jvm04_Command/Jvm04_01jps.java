3b9cf0933c90b98a215c79f025177cbe8bcae76c
HealerJean <healerjean@gmail.com>
2020-12-07 17:21:47 +0800

    Jvm修改

--- _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm04_Command/Jvm04_01jps.java
+++ _posts/8_Jvm/hlj-jvm/com-hlj-admin/src/main/java/com/hlj/moudle/Jvm04_Command/Jvm04_01jps.java
@@ -2,13 +2,14 @@ package com.hlj.moudle.Jvm04_Command;
 
 public class Jvm04_01jps {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        method();
+    }
 
-        final String s1 = new String("aa");
-        String s2 = "aabb";
-        String s3 = s1 + "bb";
-        System.out.println(s3 == s2);
+    public static void method(){
+        while (true) {
 
+        }
     }
 
 }
